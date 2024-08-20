1ada3b43acc72d55cafc9599decdb152ad0fd43d
DoNga <127024731+ngadeptrai1@users.noreply.github.com>
2024-08-20 04:38:50 +0000

    1

--- src/main/java/com/edu/authen/config/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/edu/authen/config/CustomOAuth2SuccessHandler.java
@@ -49,7 +49,7 @@ public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHa
            Cookie jwtCookie = new Cookie("123",  jwtService.generateToken(customUserDetail));
            jwtCookie.setPath("/");
            jwtCookie.setDomain("waggy-petshop.netlify.app");
-           response.setHeader("Set-Cookie", String.format("%s=%s; Path=/; HttpOnly; Secure; SameSite=Lax", jwtCookie.getName(), jwtCookie.getValue()));
+           response.setHeader("Set-Cookie", String.format("%s=%s; Path=/;domain=waggy-petshop.netlify.app; SameSite=Lax", jwtCookie.getName(), jwtCookie.getValue()));
            response.addCookie(jwtCookie);
        }
       else{
@@ -58,7 +58,7 @@ public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHa
            Cookie jwtCookie = new Cookie("123",  jwtService.generateToken(customUserDetail));
            jwtCookie.setPath("/");
            jwtCookie.setDomain("waggy-petshop.netlify.app");
-           response.setHeader("Set-Cookie", String.format("%s=%s; Path=/; HttpOnly; Secure; SameSite=Lax", jwtCookie.getName(), jwtCookie.getValue()));
+           response.setHeader("Set-Cookie", String.format("%s=%s; Path=/;domain=waggy-petshop.netlify.app; SameSite=Lax", jwtCookie.getName(), jwtCookie.getValue()));
            response.addCookie(jwtCookie);
        }
         // Redirect to frontend
